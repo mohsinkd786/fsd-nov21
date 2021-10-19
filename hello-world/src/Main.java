a7411fb02318ba5ac26efa66e6cae4b1b9fadb19
techmomo <mkhursheed@spireon.com>
2021-10-19 13:00:58 +0530

    Java Collections

--- hello-world/src/Main.java
+++ hello-world/src/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -20,20 +25,98 @@ public class Main {
         // loops
         // for loop
         for (int j = 0; j < nums1.length; j++) {
-            System.out.println(nums1[i]);
+            System.out.println(nums1[j]);
         }
 
         // while loop
-        i = nums.length;
-        while(i>0){
-            System.out.println(nums[i]);
-            i--;
-        }
+//        i = nums.length;
+//        while(i>0){
+//            System.out.println(nums[i]);
+//            i--;
+//        }
 
         // for each
         for (int k: nums) {
             System.out.println(k);
         }
+
+        /*
+        * Collections :
+        *
+        *  package : java.util
+        * List (I):
+        *   Vector
+        *   ArrayList
+        *   LinkedList
+        *
+        * Set (I):
+        *   HashSet
+        *   TreeSet
+        *
+        * Queue (I):
+        *   BlockingQueue
+        *   PriorityQueue
+        *
+        * Stack:
+        *
+        * Map:
+        *   HashMap
+        *   TreeMap
+        *
+        * */
+
+        Child child = new Child();
+        child.callMe();
+
+        StepChild stepChild = new StepChild();
+        stepChild.tryMe();
+
+        StepParent stepParent1 = new Child();
+        stepParent1.add();
+        StepParent stepParent2 = new StepChild();
+
+        Parent parent = new Child();
+        parent.sayHello();
+
+        // Parent parent1 = new Parent();
+
+        // Iterable -> Collection -> List -> ArrayList
+        // Collection (I) - parent I for Java Collections
+        // Collections (c) - utility class for java collections
+        List list = new ArrayList();
+        list.add(10);
+        list.add("String");
+        list.add(true);
+        list.add(10);
+
+        //list.remove(new Integer(10));
+        list.remove("String");
+        //
+        list.get(1);
+
+        System.out.println("LIST########");
+        for (int j = 0; j < list.size() ; j++) {
+            System.out.println(list.get(j));
+        }
+
+        //for each
+        for (Object obj:list) {
+            System.out.println(obj);
+        }
+
+        System.out.println("SET^^^^^^^");
+        Set set = new HashSet();
+        set.add(15);
+        set.add("String");
+        set.add(true);
+        set.add(15);
+        set.remove(15);
+
+        //for each
+        for (Object obj:set) {
+            System.out.println(obj);
+        }
+
     }
 }
 
@@ -51,4 +134,66 @@ public class Main {
  * decimal - float / double
  * boolean - boolean
  *
+ *
+ * Problem 1: arranges the even numbers first & then the odd numbers in sorted order .
+ *
+ * e.g. [1,2,9,8,10,5]
+ *
+ *  expected : [10,8,2,9,5,1]
+ *
+ * Problem 2: arrange the elements in the order of exact square root in sorted order.
+ *
+ * e.g. [8,9,10,15,16]
+ * roots : [2.8,3,3.3,3.6,4]
+ * expected : [4,3,3.6,2.8]
+ *
  * */
+
+interface StepParent{
+    void add();
+}
+abstract class Parent{
+
+    void add(){
+        System.out.println("Parent");
+    }
+
+    abstract void sayHello();
+}
+
+class Child extends Parent implements StepParent{
+
+    public void add(){
+        System.out.println("Child");
+    }
+    public void callMe(){
+
+    }
+
+    @Override
+    void sayHello() {
+
+    }
+}
+class StepChild implements StepParent{
+    @Override
+    public void add() {
+        System.out.println("Step Child");
+    }
+
+    public void tryMe(){
+
+    }
+}
+
+/*
+*  Tomorrows Session :
+*   Collections : Map , Set
+*   Java 8 Streams,
+*   Functional Interfaces,
+*   Lambdas,
+*   Anonymous class
+*   Default methods, Static methods
+*
+*
+* */
