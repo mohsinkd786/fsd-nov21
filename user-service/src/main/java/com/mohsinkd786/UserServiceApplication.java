c10a5e6abab9c7eb6b4c5d91584b2320208a1d5e
techmomo <mkhursheed@spireon.com>
2021-11-02 12:37:45 +0530

    problem statement

--- user-service/src/main/java/com/mohsinkd786/UserServiceApplication.java
+++ user-service/src/main/java/com/mohsinkd786/UserServiceApplication.java
@@ -11,3 +11,17 @@ public class UserServiceApplication {
 	}
 
 }
+
+
+/**
+ * Problem 1: An Organization has multiple Projects it works upon, & different employees work on those projects,
+ * calculate the total revenue for the projects , & enlist new employees in a projects , if need be.
+ *
+ *
+ * Note : Revenue for the company = Total Billables for all the projects (we can take some assumptions there).
+ *		: Profit = Total Revenue - Total Project Cost
+ *					(Project Cost will be calculated based upon the salary of employees + 10% miscl. expenses)
+ * Calculate :
+ * 	1. total revenue for the company
+ * 	2. total profit for the company
+ * */
